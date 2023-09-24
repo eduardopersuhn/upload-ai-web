1dee416703da95bfb4ac0d2b4351afa8323a99f0
Eduardo Persuhn <e.persuhn@gmail.com>
2023-09-24 17:51:56 -0300

    conclusão do frontend

---
+++ src/lib/ffmpeg.ts
@@ -0,0 +1,19 @@
+import { FFmpeg } from '@ffmpeg/ffmpeg'
+import { toBlobURL } from '@ffmpeg/util';
+
+let ffmpeg: FFmpeg | null
+
+export async function getFFmpeg() {
+  const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.2/dist/esm";
+  if (!ffmpeg) {
+    ffmpeg = new FFmpeg()
+  }
+  if (!ffmpeg.loaded) {
+    await ffmpeg.load({
+      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
+      wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, "application/wasm"),
+      workerURL: await toBlobURL(`${baseURL}/ffmpeg-core.worker.js`, "text/javascript"),
+    })
+  }
+  return ffmpeg
+}
