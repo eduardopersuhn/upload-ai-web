d8a03ed910874b9386883984bc1cf7ee17000171
Eduardo Persuhn <e.persuhn@gmail.com>
2023-09-24 22:58:40 -0300

    configurações do gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/upload-ai-web",
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
